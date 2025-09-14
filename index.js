722321df39f7a007c9df3ae4375bff84870b1442
Honey-kr <shivammahrolia@gmail.com>
2025-09-15 00:10:27 +0530

    feat: cicd pipelin

---
+++ index.js
@@ -0,0 +1,1 @@
+console.log('Hwllo, learning the cicd pipeline')
---
+++ test.js
@@ -0,0 +1,3 @@
+console.log('i am leaning using github actions, testing starts...')
+setTimeout(() => console.log("processing"), 3000)
+console.log('test complete');
